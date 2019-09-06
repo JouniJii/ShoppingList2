4d26a665a12100bdb579c4f94b136a407caa6e13
JouniJii <jouni.jaaskelainen@savonia.fi>
2019-09-06 17:30:45 +0300

    testimuutos

--- app/src/main/java/com/example/shoppinglist2/MainActivity.java
+++ app/src/main/java/com/example/shoppinglist2/MainActivity.java
@@ -2,13 +2,41 @@ package com.example.shoppinglist2;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.os.Bundle;
+import android.view.Gravity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
-// kommentti
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private Button sButton = null;
+    private Context context = null;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        sButton = findViewById(R.id.button);
+        sButton.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        if(context == null){
+            context = getApplicationContext();
+        }
+
+        Toast toast = Toast.makeText(context, "You clicked Save-button.", Toast.LENGTH_SHORT);
+        toast.setGravity(Gravity.CENTER, 0, 0);
+        toast.show();
+    }
+
+
+    @Override
+    public void onPointerCaptureChanged(boolean hasCapture) {
+
     }
 }
