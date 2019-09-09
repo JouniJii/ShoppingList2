4d9f8c481e94f7966e1fccd35862083160c38c3d
JouniJii <54974555+JouniJii@users.noreply.github.com>
2019-09-09 11:51:36 +0300

    Update MainActivity.java

--- app/src/main/java/com/example/shoppinglist2/MainActivity.java
+++ app/src/main/java/com/example/shoppinglist2/MainActivity.java
@@ -7,31 +7,60 @@ import android.os.Bundle;
 import android.view.Gravity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     private Button sButton = null;
     private Context context = null;
+    private List shoppinglist = new ArrayList();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        sButton = findViewById(R.id.button);
-        sButton.setOnClickListener(this);
+//        sButton = findViewById(R.id.button);
+//        sButton.setOnClickListener(this);
+        // Save
+        findViewById(R.id.button).setOnClickListener(this);
+        // Done
+        findViewById(R.id.button2).setOnClickListener(this);
     }
 
     @Override
     public void onClick(View view) {
+
+        int id = view.getId();
+
+        EditText eEditText = findViewById(R.id.editText2);
+        int textlength = eEditText.getText().length();
+
         if(context == null){
             context = getApplicationContext();
         }
 
-        Toast toast = Toast.makeText(context, "You clicked Save-button.", Toast.LENGTH_SHORT);
-        toast.setGravity(Gravity.CENTER, 0, 0);
-        toast.show();
+        if (id == R.id.button) {
+            if (textlength < 3 || textlength > 15) {
+                Toast toast = Toast.makeText(context, "Text length should be between 3 and 15 characters.", Toast.LENGTH_SHORT);
+                toast.setGravity(Gravity.CENTER, 0, 0);
+                toast.show();
+            }
+            else {
+                shoppinglist.add(eEditText.getText());
+                Toast toast = Toast.makeText(context, eEditText.getText() + " added to shoppinglist.", Toast.LENGTH_SHORT);
+                toast.setGravity(Gravity.CENTER, 0, 0);
+                toast.show();
+            }
+
+        }
+        if (id == R.id.button2) {
+
+        }
     }
 
 
